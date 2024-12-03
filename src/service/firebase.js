93f6a278901adb1801b62e9ecec8359244e665a6
Pedro J. <pedrojluke87@gmail.com>
2024-12-03 17:50:10 +0100

    ADD environment file and firebase config

---
+++ src/service/firebase.js
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+
+import { getAnalytics } from "firebase/analytics";
+import { initializeApp } from "firebase/app";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: Constants.expoConfig.extra.firebase.apiKey,
+  authDomain: Constants.expoConfig.extra.firebase.authDomain,
+  projectId: Constants.expoConfig.extra.firebase.projectId,
+  storageBucket: Constants.expoConfig.extra.firebase.storageBucket,
+  messagingSenderId: Constants.expoConfig.extra.firebase.messagingSenderId,
+  appId: Constants.expoConfig.extra.firebase.appId,
+  measurementId: Constants.expoConfig.extra.firebase.measurementId,
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
