0e1291165effaeadaed430edadd34b5171dc01f1
Pedro J. <pedrojluke87@gmail.com>
2025-01-15 07:30:49 +0100

    minor fix

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
-import Navigation from "./src/utils/Navigation"; // Importamos la configuración de navegación
+import Navigation from "./src/utils/Navigation";
 import { NavigationContainer } from "@react-navigation/native";
 import React from "react";
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Navigation /> {/* Utilizamos el componente Navigation */}
+      <Navigation />
     </NavigationContainer>
   );
 }
